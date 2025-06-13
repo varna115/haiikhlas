84601c0d891190d7f2cb188fe9407943ce29b246
varna115 <ezstruggle@gmail.com>
2025-06-13 19:12:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("hello selamat datang di web kami")
 st.write(
     "mari kita belajar informatika bersama sama dengan asik dan menyenangkan"
 )
+st.image("IMG-20250613-WA0155.jpg", width=200)
